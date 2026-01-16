83f2fd176479b477e90342b28bc548c540ab31de
vrjbhvsr <vrajcbhavsar0905@gmail.com>
2026-01-16 20:14:45 +0100

    Add table summarization model loader and constants
    - Introduced BaseModelLoader for defining document loader structure.
    - Implemented table_summarizer class for loading table summarization models.
    - Added relevant constants for model configuration in __init__.py.

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,4 +1,5 @@
-#================================================================#                     Loader Constants
+#================================================================#   
+#                  Loader Constants
 #================================================================
 
 OCR_LANGUAGES = "eng"
@@ -6,3 +7,15 @@ MODE = "elements"
 STRATEGY = "hi_res"
 INFER_TABLE_STRUCTURE = True
 FILE_PATH = "/home/jovyan/work/BioRAG/Data/pdfs/cells-11-02650-v2.pdf"
+
+
+#================================================================#   
+#                  Table summariser Constants
+#================================================================   
+
+MODEL_NAME = "Qwen/Qwen2.5-7B-Instruct"
+MAX_NEW_TOKENS = 512
+DEVICE_MAP = "auto"
+DTYPE = "auto"
+DO_SAMPLE = False
+SKIP_PROMPT = True
---
+++ src/models/table_summariser.py
@@ -0,0 +1,51 @@
+from models.base import BaseModelLoader
+from config.logging import log
+from langchain_core.runnables.base import RunnableBinding
+from config.exception import CustomException
+from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
+from constants import *
+from dotenv import load_dotenv
+import sys
+
+logger = log()
+log = logger.get_logger(__name__)
+# defining environment variable
+load_dotenv()
+
+
+class table_summarizer(BaseModelLoader):
+    """
+    A model loader for table summarization tasks.
+    """
+    def __init__(self):
+        self.model_name = "Qwen/Qwen2.5-7B-Instruct"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        
+
+    def load(self) -> RunnableBinding:
+        """
+        Load the table summarization model.
+
+        Returns:
+            RunnableBinding: A runnable binding for the table summarization model.
+        """
+        try:
+            # Placeholder for actual model loading logic
+            log.info("Loading table summarization model...")
+            pipe = pipeline("text-generation",
+                    tokenizer=self.tokenizer,
+                   model= self.model_name,
+                    device_map = DEVICE_MAP,
+                    dtype = DTYPE,
+                    max_new_tokens=MAX_NEW_TOKENS,   
+                    do_sample=DO_SAMPLE,
+                   )
+            model_pipe = HuggingFacePipeline(pipeline=pipe)
+            model = model_pipe.bind(skip_prompt = SKIP_PROMPT)
+            log.info("Table summarization model loaded successfully.")
+            return model
+        except Exception as e:
+            log.error("Error loading table summarization model.")
+            raise CustomException(e, sys)
+
